aa34066f5e21ee630858bdd564c9c943da6e1022
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-02 21:39:35 +0900

    Add: カテゴリー作成画面 作成

---
+++ categories/new.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP DB</title>
+</head>
+
+<body>
+  <h3>Categories - New</h3>
+  <hr>
+  <p>ID: <input type="text" name="id"></p>
+  <p>TITLE: <input type="text" name="title"></p>
+  <p><input type="submit" value="SAVE"></p>
+<hr>
+<a href="index.php">BACK</a>
+</body>
+
+</html>
