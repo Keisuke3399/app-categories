a88916fe5d81a08cb3dc51d7fe282977b9e65811
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-02 23:25:25 +0900

    Add: form POSTリクエストを追加

--- categories/new.php
+++ categories/new.php
@@ -9,9 +9,11 @@
 <body>
   <h3>Categories - New</h3>
   <hr>
+  <form action="create.php" method="post">
   <p>ID: <input type="text" name="id"></p>
   <p>TITLE: <input type="text" name="title"></p>
   <p><input type="submit" value="SAVE"></p>
+  </form>
 <hr>
 <a href="index.php">BACK</a>
 </body>
