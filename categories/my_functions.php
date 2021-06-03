73e8eac67edc2c5d1b65afd1820f40e59998e6a4
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-03 12:08:53 +0900

    Add:

--- categories/my_functions.php
+++ categories/my_functions.php
@@ -2,11 +2,11 @@
 
 function new_pdo()
 {
-  $options = [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false
-  ];
-  $pdo = new PDO("mysql:host=localhost;dbname=nakadb", "root", "root", $options);
-  return $pdo;
+    $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+    $pdo = new PDO("mysql:host=localhost;dbname=nakadb", "root", "root", $options);
+    return $pdo;
 }
