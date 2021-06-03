6b7542edf888cd576024184b58cec041d9342dac
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-03 12:23:55 +0900

    Add: index.php(一覧)からGetクエリパラメータを受け取り、主キーを元に初期画面設定/ update.php(上書きsave)へform Postリクエスト

--- categories/edit.php
+++ categories/edit.php
@@ -1,1 +1,48 @@
 <?php
+
+$id = (int)filter_input(INPUT_GET, "id");
+
+try {
+  require_once("my_functions.php");
+  $pdo = new_pdo();
+
+  $sql = "select * from categories where id = :id";
+  $ps = $pdo->prepare($sql);
+  $ps->bindValue(":id", $id, PDO::PARAM_INT);
+  $ps->execute();
+  $category = $ps->fetch();
+
+  if ($category === false) {
+    error_log("Invalid id. $id");
+    header("Location: error.php");
+    exit();
+  }
+} catch (PDOException $e) {
+  error_log("PDOException: " . $e->getMessage());
+  header("Location: error.php");
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP DB</title>
+</head>
+
+<body>
+  <h3>Categories - Edit</h3>
+  <hr>
+  <form action="update.php" method="post">
+  <input type="hidden" name="id" value="<?= htmlspecialchars($category['id']) ?>">
+  <p>ID: <?= htmlspecialchars($category['id']) ?></p>
+  <p>NAME: <input type="text" name="title" value="<?= htmlspecialchars($category['title']) ?>"></p>
+  <p><input type="submit" value="SAVE"></p>
+  </form>
+  <hr>
+  <a href="index.php">BACK</a>
+</body>
+
+</html>
