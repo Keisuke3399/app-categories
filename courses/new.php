9a26836e1893d8d62cf29ddf9cc5fc9522bacda1
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-04 22:09:10 +0900

    Add: 新規画面(new.php)を作成

---
+++ courses/new.php
@@ -0,0 +1,47 @@
+<?php
+require_once("../categories/my_functions.php");
+
+try {
+  $pdo = new_pdo();
+
+  $sql = "select id, title from categories order by id";
+
+  $st = $pdo->query($sql);
+  $categories = $st->fetchAll();
+} catch (PDOException $e) {
+  error_log("PDOException: " . $e->getMessage());
+  exit();
+}
+// var_dump($categories)
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP DB</title>
+</head>
+
+<body>
+  <h3>Courses - New</h3>
+  <hr>
+  <form action="create.php" method="post" <p>ID: <input type="number" name="id"></p>
+    <p>NAME: <input type="text" name="title"></p>
+    <p>LEARNING_TIME: <input type="number" name="learning_time"></p>
+    <p>CATEGORY:
+      <select name="category_id">
+        <?php foreach ($categories as $category) { ?>
+          <option value="<?= htmlspecialchars($category['id']) ?>">
+            <?= htmlspecialchars($category['title']) ?>
+          </option>
+        <?php } ?>
+      </select>
+    </p>
+    <button type="submit">SAVE</button>
+  </form>
+  <hr>
+  <a href="index.php">BACK</a>
+</body>
+
+</html>
