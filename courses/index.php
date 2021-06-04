bd124d675e35ffe6ab8c75ec2ed07e05f330fdfc
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-04 22:08:25 +0900

    Add: 新規画面(new.php)を作成

--- courses/index.php
+++ courses/index.php
@@ -1,13 +1,14 @@
 <?php
+require_once("../categories/my_functions.php");
+
 try {
-  require_once("../categories/my_functions.php");
   $pdo = new_pdo();
 
-  $sql = "select co.id, co.title, co.learning_time, ca.title category_title 
-  from courses co left join categories ca on co.category_id = ca.id order by co.id";
+  $sql = "select co.id, co.title, co.learning_time, ca.title category_title
+    from courses co left join categories ca on co.category_id = ca.id order by co.id";
+
   $st = $pdo->query($sql);
   $courses = $st->fetchAll();
-
 } catch (PDOException $e) {
   error_log("PDOException: " . $e->getMessage());
   header("Location: error.php");
@@ -33,6 +34,8 @@ try {
       <th>TITLE</th>
       <th>LEARNING_TIME</th>
       <th>CATEGORY</th>
+      <th>EDIT</th>
+      <th>DELETE</th>
     </tr>
     <?php foreach ($courses as $course) { ?>
       <tr>
@@ -40,9 +43,13 @@ try {
         <td><?= htmlspecialchars($course['title']) ?></td>
         <td><?= htmlspecialchars($course['learning_time']) ?></td>
         <td><?= htmlspecialchars($course['category_title']) ?></td>
+        <td><a href="edit.php?id=<?= $course['id'] ?>">EDIT</a></td>
+        <td><a href="delete.php?id=<?= $course['id'] ?>">DELETE</a></td>
       </tr>
     <?php } ?>
   </table>
+  <hr>
+  <a href="new.php">NEW</a>
 </body>
 
 </html>
