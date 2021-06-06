ed8c3a98d7020b410f731237325d2923c8a4627e
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-06 13:45:38 +0900

    Add: ・ログイン画面入力後の画面の処理（login.php）HTML/PHP
    入力ボックスにログインしたユーザーのID/NAMEを表示する。
    1. 画面から送信されたPOSTリクエストパラメータを取得する
    2. データベースに接続する
    3. SQL（select文）を定義する
    4. SQLのプレースホルダにリクエストパラメータをバインドする
    5. SQLを実行する
    6. userテーブルを使ってログイン可能か確認
    ・[ログイン成功した場合]
      ・画面に"OK"と表示する
    ・[ログインに失敗した場合]
      ・画面に"NG"と表示する

---
+++ login/login.php
@@ -0,0 +1,42 @@
+<?php
+require_once("../categories/my_functions.php");
+
+$id = (int)filter_input(INPUT_POST, "id");
+$password = (string)filter_input(INPUT_POST, "password");
+
+try {
+  $pdo = new_pdo();
+  $sql = "select * from users where id = :id and password = :password";
+
+  $ps = $pdo->prepare($sql);
+  $ps->bindValue(":id", $id, PDO::PARAM_INT);
+  $ps->bindValue(":password", $password, PDO::PARAM_STR);
+  $ps->execute();
+  $user = $ps->fetch();
+// var_dump($user);
+// die('test');
+} catch (PDOException $e) {
+  error_log("PDOException: " . $e->getMessage());
+  // exit();
+}
+  $message = "NG";
+if ($user !== false) {
+  $message = "OK";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <?= $user['id'] ?>
+  <?= $user['name'] ?>
+
+  <?= $message ?>
+</body>
+</html>
