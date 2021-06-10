f2cdf20ca8feba756a1a97ab950a773a719d2b55
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-10 21:48:06 +0900

    Add: 検索追加 learning_time 1つ追加

--- search/search_result.php
+++ search/search_result.php
@@ -3,30 +3,43 @@ require_once("../categories/my_functions.php");
 
 $title_q = (string)filter_input(INPUT_GET, "title_q");
 $lt_q_min = (int)filter_input(INPUT_GET, "lt_q_min");
+$lt_q_max = (int)filter_input(INPUT_GET, "lt_q_max");
 // var_dump($title_q);
 // var_dump($lt_q_min);
+// var_dump($lt_q_max);
 // die('debug');
+try {
+  $pdo = new_pdo();
+  // var_dump($pdo);
 
-$pdo = new_pdo();
-// var_dump($pdo);
+  $sql = "select id, title, learning_time from courses where title like :title_q";
+  if ($lt_q_min !== 0 && $lt_q_max !== 0) {
+    $sql .= " and learning_time between :lt_q_min and :lt_q_max";
+  } else if ($lt_q_min !== 0) {
+    $sql .= " and learning_time = :lt_q_min";
+  } else if ($lt_q_max !== 0) {
+    $sql .= " and learning_time = :lt_q_max";
+  }
+  // var_dump($sql);
+  $ps = $pdo->prepare($sql);
+  $title_q = "%" . $title_q . "%";
+  $ps->bindValue(":title_q", $title_q, PDO::PARAM_STR);
 
-$sql = "select id, title, learning_time from courses where title like :title_q";
-if ($lt_q_min !== 0) {
-  $sql .= " and learning_time = :lt_q_min";
+  if ($lt_q_min !== 0 && $lt_q_max !== 0) {
+    $ps->bindValue(":lt_q_min", $lt_q_min, PDO::PARAM_INT);
+    $ps->bindValue(":lt_q_max", $lt_q_max, PDO::PARAM_INT);
+  } else if ($lt_q_min !== 0) {
+    $ps->bindValue(":lt_q_min", $lt_q_min, PDO::PARAM_INT);
+  } else if ($lt_q_max !== 0) {
+    $ps->bindValue(":lt_q_max", $lt_q_max, PDO::PARAM_INT);
+  }
+  // var_dump($ps);
+
+  $ps->execute();
+  $course_search = $ps->fetchAll();
+} catch (PDOException $e) {
+  error_log("PDOException :" . $e->getMessage());
 }
-// var_dump($sql);
-
-$ps = $pdo->prepare($sql);
-$title_q = "%" . $title_q . "%";
-$ps->bindValue(":title_q", $title_q, PDO::PARAM_STR);
-
-if ($lt_q_min !== 0) {
-  $ps->bindValue(":lt_q_min", $lt_q_min, PDO::PARAM_INT);
-}
-var_dump($ps);
-
-$ps->execute();
-$course_search = $ps->fetchAll();
 
 // var_dump($course_search);
 
@@ -43,8 +56,10 @@ $course_search = $ps->fetchAll();
 <body>
   <h3>Course Search</h3>
   <hr>
-  <p> <input type="text" name="title_q" value="<?= $_GET['title_q'] ?>">
-    <input type="number" name="lt_q_min" value="<?= $_GET['lt_q_min'] ?>">
+  <p>
+    TITLE <input type="text" name="title_q" value="<?= $_GET['title_q'] ?>">
+    LT <input type="number" name="lt_q_min" value="<?= $_GET['lt_q_min'] ?>"> 〜
+    <input type="number" name="lt_q_max" value="<?= $_GET['lt_q_max'] ?>">
     <button type="submit">search</button>
   </p>
   <table border="1">
