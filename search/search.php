24be63b6f9470eaeaad745529fbbfe3389740b7f
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-11 23:11:37 +0900

    Add: 全ての検索をテキストボックスに入力した場合の処理追加
    データベースから取得してセレクトボックスを表示するためhtmlファイルを削除、phpファイルに変更作成

---
+++ search/search.php
@@ -0,0 +1,43 @@
+<?php
+require_once("../categories/my_functions.php");
+
+try {
+  $pdo = new_pdo();
+  $sql = "select title from categories";
+
+  $st = $pdo->query($sql);
+  $categories = $st->fetchAll();
+} catch (PDOException $e) {
+  error_log("PDOException: " . $e->getMessage());
+  exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Document</title>
+</head>
+
+<body>
+  <h3>Course Search</h3>
+  <hr />
+  <form action="search_result.php" method="get">
+    TITLE <input type="text" name="co_title" /> LT
+    <input type="number" name="lt_q_min" /> 〜
+    <input type="number" name="lt_q_max" />
+    <select name="ca_title">
+      <?php foreach ($categories as $category) { ?>
+        <option value="<?= htmlspecialchars($category['title']) ?>">
+          <?= htmlspecialchars($category['title']) ?>
+        </option>
+      <?php } ?>
+    </select>
+    <button type="submit">search</button>
+  </form>
+</body>
+
+</html>
